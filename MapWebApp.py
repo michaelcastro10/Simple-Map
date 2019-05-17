0a004b1c746a7576b988d5062595ecb92881571d
michaelcastro10 <42814726+michaelcastro10@users.noreply.github.com>
2019-05-16 23:58:56 -0400

    Add files via upload

---
+++ MapWebApp.py
@@ -0,0 +1,44 @@
+import folium
+import pandas
+
+data = pandas.read_csv("Volcanoes.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+elev = list(data["ELEV"])
+
+data = pandas.read_csv("Airports.txt")
+lat1 = list(data["LAT"])
+lon2 = list(data["LON"])
+
+def color_producer(elevation):
+    if elevation < 1000:
+        return 'green'
+    elif 1000 <= elevation < 3000:
+        return 'orange'
+    else:
+        return 'red'
+map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Mapbox Bright")
+
+fgv = folium.FeatureGroup(name="Volcanoes")
+
+for lt, ln, el in zip(lat, lon, elev):
+    fgv.add_child(folium.CircleMarker(location=[lt, ln], radius = 6, popup=str(el)+" m",
+    fill_color=color_producer(el), fill=True,  color = 'grey', fill_opacity=0.7))
+
+flv = folium.FeatureGroup(name="Airports")
+
+for lx, ly, in zip(lat1, lon2):
+    flv.add_child(folium.Marker(location=[lx, ly], popup="Hi, I'm an Airport!", icon=folium.Icon(color='green')))
+
+fgp = folium.FeatureGroup(name="Population")
+
+fgp.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
+style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000
+else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
+
+map.add_child(fgv)
+map.add_child(flv)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
+
+map.save("index.html")
